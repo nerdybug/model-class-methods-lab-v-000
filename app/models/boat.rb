e324ba407e4df9a057dc31b3b1496281339d6d27
Meredith Reece <mardebugger@gmail.com>
2018-08-03 02:14:00 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -30,4 +30,8 @@ class Boat < ActiveRecord::Base
   def self.with_three_classifications
     joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
   end
+
+  def self.longest
+    Boat.order(length: :desc).first
+  end
 end
